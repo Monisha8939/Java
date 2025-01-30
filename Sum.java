bdc25793a3a6d740df0937ba9cf70f5e96399198
MONISHA K <k48172473@gmail.com>
2025-01-30 23:13:25 +0530

    Create Sum.java

---
+++ Sum.java
@@ -0,0 +1,14 @@
+//sum of two numbers 
+import java.util.Scanner;
+class A
+{
+  public static void main(String args[])
+  {
+    Scanner s=new Scanner(System.in);
+    int a=s.nextInt();//get the input from the user
+    int b=s.nextInt();//get the input from the user
+    int c=a+b;//add two numbers
+    System.out.println(c);
+  }
+}
+    
