5c3ad9655eb9cbcf2290a8071740cba6172b1e1f
MONISHA K <k48172473@gmail.com>
2025-02-19 20:27:39 +0530

    Update ASCII value.java

--- ASCII value.java
+++ ASCII value.java
@@ -2,14 +2,14 @@ import java.util.Scanner;
 
 public class ASCIIValue {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner s = new Scanner(System.in);
       
-        char ch = scanner.next().charAt(0); // Get character input from user
+        char ch = s.next().charAt(0); // Get character input from user
       
         int ascii = (int) ch; // Display ASCII value
         System.out.println("ASCII value of '" + ch + "' is: " + ascii);
 
-        scanner.close();
+        s.close();
     }
 }
   
