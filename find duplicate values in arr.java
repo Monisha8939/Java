af78d48d960bda2e179aea54f270bf4d059d5a05
MONISHA K <k48172473@gmail.com>
2025-02-20 23:04:52 +0530

    duplicate values in arr.java

--- find duplicate values in arr.java
+++ find duplicate values in arr.java
@@ -23,3 +23,36 @@ class A
         }
     }
   }
+// remove the duplicates 
+import java.util.Scanner;
+class A
+  {
+  public static void main(String args[])
+    {
+  Scanner s=new Scanner(System.in);
+  int visited=-1;
+  int n=s.nextInt();
+  int arr[]=new int[n];
+    for(int k=0;k<arr.length;k++)
+      {
+        arr[k]=s.nextInt();
+      }
+    for(int i=0;i<arr.length;i++)
+      {
+        if(arr[i]!=-1)
+        {
+        for(int j=i+1;j<arr.length;j++)
+          {
+            if(arr[i]==arr[j])
+            {
+              arr[j]=visited;
+            }
+          }
+          System.out.print(arr[i]+" ");
+        }
+      }
+  }
+}
+      
+    
+  
