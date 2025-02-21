a21ef55d7a61f8c84e530de957c80e3ea9e0e729
MONISHA K <k48172473@gmail.com>
2025-02-21 22:59:08 +0530

    Sort 0,1,2.java

--- Sort 0,1,2.java
+++ Sort 0,1,2.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
     int n=sc.nextInt();
     int arr[]=new int[n];
     for(int i=0;i<n;i++)
-    {
+     {
       arr[i]=sc.nextInt();
     }
     int mid=0; // points at index 0
