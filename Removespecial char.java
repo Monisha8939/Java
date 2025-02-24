493352637cfb0858e1520bb6e6ca6b9793699af5
MONISHA K <k48172473@gmail.com>
2025-02-24 21:57:01 +0530

    Removespecial char.java

--- Removespecial char.java
+++ Removespecial char.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
-
-public class RemoveNonAlphanumericManual {
+class RemoveNonAlphanumericManual {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
