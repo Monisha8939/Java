3329f4fdc29dc2b0fef28c4689b34938864bd7c4
MONISHA K <k48172473@gmail.com>
2025-02-27 22:44:59 +0530

    find duplicate word in a string.java

---
+++ find duplicate__word in a string.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        String str=s.nextLine();
+        int count;
+        str=str.toLowerCase();
+        String string[]=str.split(" ");
+        for(int i=0;i<string.length;i++)
+        {
+            count=1;
+            for(int j=i+1;j<string.length;j++)
+            {
+                if(string[i].equals(string[j]))
+                {
+                    count++;
+                    string[j]="0";
+                }
+            }
+            if(count>1 && string[i]!="0")
+            {
+                System.out.println(string[i]);
+            }
+
+        }
+    }
+
+}
