4e6d503bfabeb79f2441b22e8cb9948c5acd02f2
MONISHA K <k48172473@gmail.com>
2025-03-05 23:25:38 +0530

    Vowels.java

--- Vowels.java
+++ Vowels.java
@@ -20,3 +20,21 @@ class Vowel {
         scanner.close();
     }
 }
+// count vowels present in the string
+import java.util.Scanner;
+class A{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int vowels=0;  // Variable to count vowels
+        String str=s.nextLine(); // Read input string
+        str=str.toLowerCase();  // Convert string to lowercase
+        int len=str.length(); // Get length of the string
+        for(int i=0;i<len;i++){
+            if(str.charAt(i)=='a'||str.charAt(i)=='e'||str.charAt(i)=='i'||str.charAt(i)=='o'||str.charAt(i)=='u'){ // Check if character is a vowel
+                vowels++; // Increment vowel count
+            }
+        }
+            System.out.println(vowels); // Print the total number of vowels
+        
+    }
+}
