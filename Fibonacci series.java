1dc9d23d01183737c9a806e5f1977bb9f32206ff
MONISHA K <k48172473@gmail.com>
2025-03-06 21:51:33 +0530

    Fibonacci series.java

--- Fibonacci series.java
+++ Fibonacci series.java
@@ -4,7 +4,7 @@ class  {
         Scanner s = new Scanner(System.in);
         int first = s.nextInt();
         int second = s.nextInt();
-        int n = s.nextInt(); 
+        int n = s.nextInt();
         int next = 0; // store the variable is next
       
         // Loop to generate Fibonacci series up to 'n' terms
