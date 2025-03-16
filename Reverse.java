7a70cc1d5472635cbd31028d78dca5686b898140
MONISHA K <k48172473@gmail.com>
2025-03-16 20:58:33 +0530

    Reverse a number.java

--- Reverse.java
+++ Reverse.java
@@ -16,21 +16,4 @@ class Reverse
       System.out.println(ans);
     }
   }
-//reverse a string
-import java.util.Scanner;
-class Reverse
-  {
-    public static void main(String arrgs[])
-    {
-      Scanner s=new Scanner(System.in);
-      String str=s.nextLine();//get the input from the user
-      String str1=""; // Initialize an empty string to store the reversed string
-      for(int i=arr.length()-1;i>=0;i--)  // Loop to reverse the string
-        {
-        str1 + =str.charAt(i); // Append each character in reverse order
-        }
-      System.out.println(str1);
-    }
-  }
-        
     
