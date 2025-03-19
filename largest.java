52cedb1c9bdab73dfac792ec0aff84f5cbfd0657
MONISHA K <k48172473@gmail.com>
2025-03-19 23:07:45 +0530

    largest.java

--- largest.java
+++ largest.java
@@ -21,3 +21,23 @@ class largest
       }
     }
   }
+//find the largest digit
+import java.util.Scanner;
+public class LargestDigit {
+	public static void main(String args[]) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int largest=0;
+		while(n>0) {
+			int digit=n%10;
+			if(digit>largest) {
+				largest=digit;
+			}
+				n=n/10;
+		}
+		System.out.println(largest);
+		
+		
+	}
+
+}
