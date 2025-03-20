00f1a90f038ef6879237e14a751f8c608ec0e51b
MONISHA K <k48172473@gmail.com>
2025-03-20 20:59:44 +0530

    largest.java

--- largest.java
+++ largest.java
@@ -4,9 +4,9 @@ class largest
     public static void main(String args[])
     {
       Scanner s=new Scanner(System.in);
-      int a=s.nextInt();
-      int b=s.nextInt();
-      int c=s.nextInt();
+      int a=s.nextInt(); //get the input for first number
+      int b=s.nextInt(); //get the input for second number
+      int c=s.nextInt(); //get the input for third number
       // Check which number is the largest
       if(a>b && a>c)
       {
@@ -26,16 +26,16 @@ import java.util.Scanner;
 public class LargestDigit {
 	public static void main(String args[]) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt();
-		int largest=0;
-		while(n>0) {
-			int digit=n%10;
-			if(digit>largest) {
-				largest=digit;
+		int n=s.nextInt();  // Read input number
+		int largest=0; // Initialize largest digit as 0 
+		while(n>0) { // Loop through each digit of the number
+			int digit=n%10; // Extract the last digit
+			if(digit>largest) { check the condition
+				largest=digit; //store the largest value
 			}
-				n=n/10;
+				n=n/10;//remove the last digit from the number
 		}
-		System.out.println(largest);
+		System.out.println(largest); //print the result
 		
 		
 	}
