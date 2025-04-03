2bd53011d2d3c6121dcf2f5d46e2ecb6aac77025
MONISHA K <k48172473@gmail.com>
2025-04-03 16:19:21 +0530

    substring.java

---
+++ string/substring.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class A{
+  public static void main(String args[])
+  {
+    Scanner s=new Scanner(System.in);
+    String str=s.nextLine();
+    for(int i=0;i<str.length();i++) // outer loop selects the starting index
+      {
+        for(int j=i+1;j<str.length;j++) // Inner loop selects the ending index
+          {
+            System.out.println(str.substring(i,j)); // Print substring from index i to j-1
+          }
+      }
+  }
+}
