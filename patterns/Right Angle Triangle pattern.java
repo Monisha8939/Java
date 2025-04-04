e156aa32c944262f7eac1b5ca6a529a1bf6d6bc4
MONISHA K <k48172473@gmail.com>
2025-04-04 22:36:06 +0530

    Right Angle Triangle pattern.java

---
+++ patterns/Right Angle Triangle pattern.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class RightAngleTriangle {
+    public static void main(String args[]) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        for (int i = 1; i <= n; i++) //Outer loop for row
+        { 
+            for (int j = 1; j <= i; j++) //Inner loop for column 
+            {  
+                System.out.print("*"); //print th * without a new line
+            }
+            System.out.println();
+        }
+    }
+}
