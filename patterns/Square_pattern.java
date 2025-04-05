f38aa00f997ab135028899c8829787200e006ea2
MONISHA K <k48172473@gmail.com>
2025-04-05 21:14:43 +0530

    SquarePattern.java

---
+++ patterns/Square_pattern.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class A{
+  public static void main(String args[]){
+    Scanner s=new Scanner(System.in);
+    int n=s.nextInt(); // Read the size of the square
+
+    for(int i=1;i<=n;i++)  // Rows
+    {
+      for(int j=1;j<=n;j++)  // Columns
+        {
+          System.out.print("*"); 
+        }
+      System.out.println();// Move to next line after each row
+    }
+    s.close();
+  }
+}
