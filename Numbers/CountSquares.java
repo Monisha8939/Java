06e7f598146f0562d30fb41d037917cae38a3708
MONISHA K <k48172473@gmail.com>
2025-04-17 23:07:11 +0530

    CountSquares.java

---
+++ Numbers/CountSquares.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class A{
+public static void main(String args[]) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int count=0;
+		int i=1;
+		while(i*i<n) //check if i*i is less than n or not 
+      {
+			count++; // If it's a square number, increase the count
+			i++; //Increment the i value
+		}
+		System.out.println(count); //Print the result
+	}
+
+}
