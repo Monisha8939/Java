c9dd22936ed286d84ef1e522441aad217e01d86c
MONISHA K <k48172473@gmail.com>
2025-04-24 23:14:21 +0530

    GCD_LCM.java

--- Numbers/GCD_LCM.java
+++ Numbers/GCD_LCM.java
@@ -9,15 +9,15 @@ class GCD_LCM {
 		fun(a,b); //Call the function
 	}
 		static void fun(int a,int b) {
-		int num1=a;
-		int num2=b;
+		int num1=a;  // Store the original values
+		int num2=b;  // Store the original values
 		while(b!=0) {
 			int temp=b;
-			b=a%b;
-			a=temp;
+			b=a%b;  // Get the remainder
+			a=temp; // Swap values
 			}
-		int gcd=a;
-		int lcm=(num1*num2)/gcd;
+		int gcd=a; // GCD is stored in 'a' after loop
+		int lcm=(num1*num2)/gcd; //Calculate the LCM using formula
 		System.out.println("gcd is:" +" "+gcd);
 		System.out.println("lcm is:" +" "+lcm);
 		
