c4c9c4c625a4d27bf5383b96cd4831ae88fb8131
MONISHA K <k48172473@gmail.com>
2025-04-25 22:08:44 +0530

    palindrome.java

---
+++ string/palindrome.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+class A{
+  public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    String s=sc.nextLine();
+    int left=0;
+    int right=s.length()-1;
+    boolean ispalindrome=true;
+    while(left<right){
+      if(s.charAt(left)!=s.charAt(right)){
+        ispalindrome=false;
+        break;
+      }
+        left++;
+      right--;
+    }
+    if(ispalindrome){
+        System.out.println("palindrome");
+    }
+      else{
+        System.out.println("this is not a palindrome");
+       
+      }
+    }
+  
+}
+    
+      
+    
