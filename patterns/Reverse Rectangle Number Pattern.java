ea5ee84b5c17badbb41f7de8c39f97530dabae82
MONISHA K <k48172473@gmail.com>
2025-04-29 22:53:40 +0530

    Reverse Rectangle Number Pattern.java

---
+++ patterns/Reverse Rectangle Number Pattern.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class A{
+public static void main(String[] args) {
+  Scanner s.new Scanner(System.in);
+  int n=s.nextInt();
+	        for (int row = n; row >= 1; row--) //Outer loop
+          {
+	            for (int num = n; num >= 1; num--) //Middle loop
+                {
+	                for (int count = 1; count <= row; count++) //Inner loop
+                  {
+	                    System.out.print(num + " ");
+	                }
+	            }
+	            System.out.println();
+	        }
+	    }
+	}
