8216ed13b16119f2e00f2d9ffe98cefa0990390c
MONISHA K <k48172473@gmail.com>
2025-05-01 21:45:27 +0530

    RemoveDuplicates.java

--- Array/RemoveDuplicates.java
+++ Array/RemoveDuplicates.java
@@ -1,21 +1,27 @@
 import java.util.Scanner;
-class A{
-  public static void main(String args[]){
+class A
+{
+  public static void main(String args[])
+  {
     Scanner s=new Scanner(System.in);
     int n=s.nextInt();
     int arr[]=new int[n];
-    for(int i=0;i<arr.length;i++){
+    for(int i=0;i<arr.length;i++)
+    {
       arr[i]=s.nextInt();
     }
-     Arrays.sort(arr);
-    int i=0;
-    for(int j=1;j<arr.length;j++){
+     Arrays.sort(arr); // Sort the array 
+    
+    int i=0;  // Use two pointers to remove duplicates
+    for(int j=1;j<arr.length;j++) 
+    { 
       if(arr[j]!=arr[i]){
        i++;
-        arr[i]=arr[j];
+        arr[i]=arr[j]; // Copy unique element forward
       }
     }
-    for(int k=0;k<=i;k++){
+    for(int k=0;k<=i;k++)   // Print the array with unique elements 
+    {
     System.out.println(arr[k]);
     }
   }
