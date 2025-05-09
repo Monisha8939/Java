3f954712582e27f5fb442101c92964f92b415889
MONISHA K <k48172473@gmail.com>
2025-05-09 20:39:54 +0530

    RemoveVowels.java

--- string/RemoveVowels.java
+++ string/RemoveVowels.java
@@ -3,9 +3,10 @@ public class remove_Vowels {
 	public static void main(String args[]) {
     Scanner sc=new Scanner(System.in);
 		String s=sc.nextLine();
-		StringBuilder result=new StringBuilder();
-		for(char ch:s.toCharArray()) {
-			if(!"aeiouAEIOU".contains(String.valueOf(ch))) {
+		StringBuilder result=new StringBuilder();  // Loop through each character in the string
+		for(char ch:s.toCharArray()) { 
+			if(!"aeiouAEIOU".contains(String.valueOf(ch))) // If the character is NOT a vowel
+			{
 				result.append(ch);
 			}
 			
