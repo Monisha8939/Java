f362db47255e89b41624267db04d88c2ded47a0c
MONISHA K <k48172473@gmail.com>
2025-05-21 20:51:12 +0530

    Count_the_character.java

---
+++ string/Count_the_character.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class A{
+  public static void main(String args[]){
+    Scanner s=new Scanner(System.in);
+    String sc=s.nextLine();
+    int upper=0,lower=0,digit=0,special=0;
+    for(int i=0;i<sc.length();i++){
+      char ch=sc.charAt(i);
+      if(Character.isUpperCase(ch)){
+        upper++;
+      }
+      else if(Character.isLowerCase(ch)){
+        lower++;
+      }
+      else if(Character.isDigit(ch)){
+        digit++;
+      }
+      else{
+        special++;
+      }
+    }
+    System.out.println("upper case is:"+upper);
+    System.out.println("lower case is:"+lower);
+    System.out.println("digit is:"+digit);
+    System.out.println("special character is:"+special);
+  }
+}
