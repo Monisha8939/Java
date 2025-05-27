4375a4e93c31c5a477774f069358625dda55b37e
MONISHA K <k48172473@gmail.com>
2025-05-27 22:00:48 +0530

    middleofThree.java

--- Numbers/middleofThree.java
+++ Numbers/middleofThree.java
@@ -2,19 +2,20 @@ import java.util.Scanner;
 class A{
   public static void main(String args[]){
     Scanner s=new Scanner(System.in);
+    // Read three integers from the user
     int a=s.nextInt();
     int b=s.nextInt();
     int c=s.nextInt();
     int middle;
-    if((a>=b && a<=c)||(a<=b && a>=c)){
+    if((a>=b && a<=c)||(a<=b && a>=c)){  // Check if 'a' is the middle value
       middle=a;
       System.out.println(middle+" "+"a is the middle number");
     }
-    else if((b>=a && b<=c)||(b<=a && b>=c)){
+    else if((b>=a && b<=c)||(b<=a && b>=c)){  // Check if 'b' is the middle value
       middle=b;
       System.out.println(middle+" "+"b is the middle number");
         }
-    else{
+    else{  // Otherwise, 'c' is the middle value
       middle =c;
       System.out.println(middle+" "+"c is the middle number");
     }
