75d6fd22b359a42225db47f3d47238e7fa861841
MONISHA K <k48172473@gmail.com>
2025-06-03 22:34:00 +0530

    DateTimeFormatter.java

---
+++ date and time/DateTimeFormatter.java
@@ -0,0 +1,10 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+class A{
+  public static void main(String args[]){
+    LocalDateTime two=LocalDateTime.now();
+    System.out.println(two);
+    DateTimeFormatter three=DateTimeFormatter.ofPattern("yyyy MMM dd ,E HH:mm:ss");
+    System.out.println(three.format(two));
+  }
+}
