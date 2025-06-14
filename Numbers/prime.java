7897a224047b72d1981ac0ae86fc85eb252d8f83
MONISHA K <k48172473@gmail.com>
2025-06-14 22:32:03 +0530

    prime.java

--- Numbers/prime.java
+++ Numbers/prime.java
@@ -4,21 +4,19 @@ class prime{
   {
     Scanner s=new Scanner(System.in);
     int n=s.nextInt(); //get the input from the user
-    int count=0;
-    for(int i=2;i<n;i++)
-      {
-        if(n%i==0) // If n is divisible, it's not a prime number
-        {
-          count++;  // Increase count if a factor is found
-        }
+    if (n <= 1) {
+      System.out.println("Not a prime");
+      return;
+    }
+
+    for (int i = 2; i <= Math.sqrt(n); i++) {
+      if (n % i == 0) {
+        System.out.println("Not a prime");
+        return;
       }
-     if(count==0) // If no factors were found, the number is prime
-        {
-          System.out.println("Prime");
-        }
-        else{
-          System.out.println("not a prime");
-        }
+    }
+
+    System.out.println("Prime");
   }
 }
   
