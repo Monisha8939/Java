27a60356549273e5e3be0353a83e0635899eb7f6
MONISHA K <k48172473@gmail.com>
2025-06-18 21:37:41 +0530

    Stepping_Number.java

--- Numbers/Stepping_Number.java
+++ Numbers/Stepping_Number.java
@@ -1,1 +1,25 @@
+import java.util.Scanner;
+class A{
+  public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    String s=Integer.toString(n);
+		boolean stepping=true;
+		for(int i=0;i<s.length()-1;i++) {
+			int a=s.charAt(i)-'0';
+			int b=s.charAt(i+1)-'0';
+			int diff=Math.abs(a-b);
+			if(diff!=1) {
+			   stepping=false;
+			}
+		}
+		if(stepping) {
+			System.out.println("Yes");
+		}
+		else {
+			System.out.println("No");
+		}
+	}
+
+}
 
