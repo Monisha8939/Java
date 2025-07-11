ccc1e073178c3f2b1ee4441540ea6aa84f191ccd
MONISHA K <k48172473@gmail.com>
2025-07-11 19:46:00 +0530

    Find_freqOfArray.java

--- Array/Find_freqOfArray.java
+++ Array/Find_freqOfArray.java
@@ -1,1 +1,19 @@
-
+import java.util.*;
+class A{
+  public static void main(String args[]){
+    Scanner s=new Scanner(System.in);
+    int n=s.nextInt();
+    int[] arr=new int[n];
+    for(int i=0;i<n;i++){
+      arr[i]=s.nextInt();
+    }
+    Map<Integer,Integer> map=new HashMap<>();
+    for(int num:arr){
+      map.put(num,map.getOrDefault(num,0)+1);
+    }
+    for(Map.Entry<Integer,Integer>entry:map.entrySet()){
+      System.out.println(entry.getKey()+"="+entry.getValue());
+    }
+  }
+}
+      
