7fc81a6de5bf2611eca992b6f891653658fb4ce8
MONISHA K <k48172473@gmail.com>
2025-07-14 20:07:11 +0530

    LengthOfLongestSubstringWithoutRepeatedCharacterf.java

--- string/LengthOfLongestSubstringWithoutRepeatedCharacterf.java
+++ string/LengthOfLongestSubstringWithoutRepeatedCharacterf.java
@@ -1,4 +1,26 @@
 import java.util.*;
-class A{
-  public static void main(String args[]){
-    
+public class qns_116 {
+	public static int longestsubstring(String str) {
+		Map<Character,Integer> map=new HashMap<>();
+		int maxlen=0;
+		int start=0;
+		
+		for(int i=0;i<str.length();i++) {
+	        char ch=str.charAt(i);
+	        
+	        if(map.containsKey(ch) && map.get(ch)>=start) {
+	        	start=map.get(ch)+1;
+	        }
+	        map.put(ch, i);
+	        maxlen=Math.max(maxlen, i-start+1);
+		}
+		return maxlen;
+}
+	public static void main(String args[]) {
+		String str="abcdefabcbb";
+		System.out.println(longestsubstring(str));
+	}
+	
+}
+
+
