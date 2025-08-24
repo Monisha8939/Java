830b8f899ffa39520f52c9a07ddcb371ef05f596
MONISHA K <k48172473@gmail.com>
2025-08-24 22:32:28 +0530

    Armstrong.java

--- Numbers/Armstrong.java
+++ Numbers/Armstrong.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 class armstrong {
     public static void main(String args[])
-    {
+    { 
         Scanner s=new Scanner(System.in);
         int n=s.nextInt();//get the input from the user
         int e=n;
