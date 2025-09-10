04b1c78e6e23dd7d2c5a6e1acf702a1d7d42636c
MONISHA K <k48172473@gmail.com>
2025-09-10 18:08:17 +0530

    moveZeros.java

--- Array/moveZeros.java
+++ Array/moveZeros.java
@@ -1,1 +1,34 @@
-
+import java.util.Scanner;
+class moveZeros{
+  public static void main(String args[])
+  {
+    Scanner s=new Scanner(System.in);
+    int n=s.nextInt();
+    int[] arr=new int[n];
+    for(int i=0;i<arr.length;i++)
+      {
+        arr[i]=s.nextInt();
+      }
+    ismoveingzeros(arr);
+    for(int num:arr)
+    {
+    System.out.println(num+" ");
+    }
+  }
+  public static void ismoveingzeros(int arr[])
+  {
+    int i=0;
+    for(int num:arr){
+      if(num!=0)
+      {
+        arr[i++]=num;
+      }
+      
+    }
+    while(i<arr.length)
+      {
+        arr[i++]=0;
+      }
+  }
+}
+    
