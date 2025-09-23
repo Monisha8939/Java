6c727146a15b6601efb362779ed609e9358a0e01
MONISHA K <k48172473@gmail.com>
2025-09-23 22:23:58 +0530

    palindrome.java

--- string/palindrome.java
+++ string/palindrome.java
@@ -6,7 +6,7 @@ class A{
     int left=0;
     int right=s.length()-1;
     boolean ispalindrome=true;
-    while(left<right){
+    while(left<right){ 
       if(s.charAt(left)!=s.charAt(right)){
         ispalindrome=false;
         break;
