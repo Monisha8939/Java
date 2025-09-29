bae52261b73be27879ba06e25aebad21a12d4cdc
MONISHA K <k48172473@gmail.com>
2025-09-29 22:32:44 +0530

    Bubble_Sort.java

--- Sorting_Techniques/Bubble_Sort.java
+++ Sorting_Techniques/Bubble_Sort.java
@@ -4,4 +4,31 @@ class bubble_Sort{
   {
     Scanner s=new Scanner(System.in);
     int n=s.nextInt();
+    int[] arr=new int[n];
+    for(int i=0;i<n;i++){
+      arr[i]=s.nextInt();
+    }
+   bubble(arr);
+    for(int num:arr){
+      System.out.print(num+" ");
+    }
+  }
+  public static void bubble(int[] arr)
+  {
+    int n=arr.length;
     
+    for(int i=0;i<n-1;i++){
+      boolean swapped=false;
+      for(int j=0;j<n-1-i;j++){
+        if(arr[j]>arr[j+1]){
+          int temp=arr[j];
+          arr[j]=arr[j+1];
+          arr[j+1]=temp;
+          swapped=true;
+        }
+      }
+    if(!swapped) break;
+    }
+  }
+}
+          
