0b79ea3a80b7c68d496b924b68426eb4765559c5
MONISHA K <k48172473@gmail.com>
2025-09-30 20:24:12 +0530

    Bubble_Sort.java

--- Sorting_Techniques/Bubble_Sort.java
+++ Sorting_Techniques/Bubble_Sort.java
@@ -5,7 +5,7 @@ class bubble_Sort{
     Scanner s=new Scanner(System.in);
     int n=s.nextInt();
     int[] arr=new int[n];
-    for(int i=0;i<n;i++){
+    for(int i=0;i<n;i++){ 
       arr[i]=s.nextInt();
     }
    bubble(arr);
