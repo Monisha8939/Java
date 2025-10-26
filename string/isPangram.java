7e5b8e53149f440e27766fe8ff70102d184c48c0
MONISHA K <k48172473@gmail.com>
2025-10-26 21:56:53 +0530

    Update isPangram.java

--- string/isPangram.java
+++ string/isPangram.java
@@ -1,1 +1,24 @@
-
+import java.util.*;
+class A{
+  public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    String s=sc.nextLine();
+    System.out.println(checkIsPangram(s));
+  }
+  public static boolean checkIsPangram(String s)
+  {
+    boolean[] seen=new boolean[26];
+    s=s.toLowerCase();
+    for(char ch:s.toCharArray())
+      {
+        if(ch>='a' && ch<='z'){
+          seen[ch-'a']=true;
+        }
+      }
+    for(boolean b:seen)
+      {
+        if(!b) return false;
+      }
+    return true;
+  }
+}
