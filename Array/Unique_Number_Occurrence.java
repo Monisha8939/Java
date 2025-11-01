31a1e63013324c63e39434da2d7d929e49abdba0
MONISHA K <k48172473@gmail.com>
2025-11-01 23:53:00 +0530

    Update and rename Unique_Number.java to Unique_Number_Occurrence.java

---
+++ Array/Unique_Number_Occurrence.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class A
+  {
+    public static void main(String args[])
+    {
+      Scanner s=new Scanner(System.in);
+      int n=s.nextInt();
+      int[] arr=new int[n];
+      for(int i=0;i<n;i++){
+        arr[i]=s.nextInt();
+    }
+      System.out.println(Unique(arr));
+    }
+    public static boolean Unique(int arr[]){
+      HashMap<Integer,Integer>map=new HashMap<>();
+      for(int n:arr){
+        map.put(n,map.getOrDefault(n,0)+1);
+      }
+      HashSet<Integer>set=new HashSet<>(map.values());
+      return map.size()==set.size();
+    }
+  }
