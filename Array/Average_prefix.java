12aa9b67a1b09d74abd8a0acc28b325b56b0e43d
MONISHA K <k48172473@gmail.com>
2025-11-27 20:38:09 +0530

    Average_prefix.java
    Implement average prefix calculation in Java.

--- Array/Average_prefix.java
+++ Array/Average_prefix.java
@@ -1,1 +1,29 @@
-
+import java.util.*;
+class A{
+  public static void main(String args[])
+  {
+    Scanner s=new Scanner(System.in);
+    int n=s.nextInt();
+    int[] arr=new int[n];
+    for(int i=0;i<n;i++){
+      arr[i]=s.nextInt();
+    }
+    ArrayList<Integer>res=AvgPrefix(arr,n);
+    for(int x:res){
+    System.out.print(x+" ");
+    }
+  }
+  public static ArrayList<Integer> AvgPrefix(int[] arr,int n){
+    long sum=0;
+    ArrayList<Integer>list=new ArrayList<>();
+    for(int i=0;i<n;i++){
+      sum+=arr[i];
+      int avg=(int)(sum/(i+1));
+      list.add(avg);
+    }
+    return list;
+  }
+}
+      
+                     
+  
