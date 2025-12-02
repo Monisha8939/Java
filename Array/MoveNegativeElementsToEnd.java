acd504cc168d227255722121ba9dfc7926a4ebc2
MONISHA K <k48172473@gmail.com>
2025-12-02 23:38:29 +0530

    MoveNegativeElementsToEnd.java

--- Array/MoveNegativeElementsToEnd.java
+++ Array/MoveNegativeElementsToEnd.java
@@ -1,1 +1,35 @@
+import java.util.*;
+class A{
+  public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int[] arr=new int[n];
+    for(int i=0;i<n;i++){
+      arr[i]=sc.nextInt();
+    }
+    int[] result=moveNegative(n, arr);
+    for(int i=0;i<n;i++){
+    System.out.print(result[i]);
+    }
+  }
+  public static int[] moveNegative(int n,int arr[]){
+    int[] temp=new int[n];
+    int r=0;
+    for(int i=0;i<n;i++){
+      if(arr[i]>=0)
+      {
+        temp[r++]=arr[i];
+      }
+    }
+    for(int i=0;i<n;i++){
+      if(arr[i]<0){
+        temp[r++]=arr[i];
+      }
+    }
+    return temp;
+  }
+}
+        
 
+
+    
