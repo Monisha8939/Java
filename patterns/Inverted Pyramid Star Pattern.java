b9d92a5f77af1f79edec229f17d0f453e80ef80c
MONISHA K <k48172473@gmail.com>
2025-12-10 21:41:44 +0530

    Inverted Pyramid Star Pattern code.java
    Fix indentation and formatting issues in the Inverted Pyramid Star Pattern code.

--- patterns/Inverted Pyramid Star Pattern.java
+++ patterns/Inverted Pyramid Star Pattern.java
@@ -5,7 +5,7 @@ class A{
     int n=s.nextInt();
     for(int i=1;i<=n;i++){ 
             for(int j=1;j<i;j++){
-                System.out.print(" ");
+                System.out.print(" "); 
             }
             for(int k=1;k<=(2*(n-i)+1);k++){
                 System.out.print("*");
