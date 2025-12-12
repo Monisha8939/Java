b71945c908f8fa9ae97392f42e253cef1ff07d91
MONISHA K <k48172473@gmail.com>
2025-12-12 22:51:34 +0530

    left_half_pyromid_pattern.java

--- patterns/left_half_pyromid_pattern.java
+++ patterns/left_half_pyromid_pattern.java
@@ -7,7 +7,7 @@ class A{
       for(int j=1;j<=n;j++){ // Inner loop to print spaces
         System.out.print(" ");
       }
-      for(int k=1;k<=i;k++){  // Inner loop to print stars
+      for(int k=1;k<=i;k++){  // Inner loop to print stars 
         System.out.print("*");
       }
       System.out.println();  // Move to the next line after each row
