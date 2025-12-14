b56dca8deab89464f5296a9694b5d57a5b369a3a
MONISHA K <k48172473@gmail.com>
2025-12-14 22:53:50 +0530

    SwapDiagonalsMatrix[][].java

--- Array/SwapDiagonalsMatrix[][].java
+++ Array/SwapDiagonalsMatrix[][].java
@@ -1,1 +1,26 @@
-
+import java.util.*;
+class A{
+  public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int[][] arr=new int[n][n];
+    for(int i=0;i<n;i++){
+      for(int j=0;j<n;j++){
+        arr[i][j]=sc.nextInt();
+      }
+    }
+   SwapDiagonalsMatrix(n, arr);
+     for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+  }
+  }
+  public static void SwapDiagonalsMatrix(int n,int arr[][]){
+    for(int i=0;i<n;i++){
+      int temp=arr[i][i];
+      arr[i][i]=arr[i][n-1-i];
+      arr[i][n-1-i]=temp;
+    }
+  }
