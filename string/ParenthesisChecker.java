8404c868f53f7016f84378ac7a4bc608ed13a3bb
MONISHA K <k48172473@gmail.com>
2025-12-22 23:13:22 +0530

    ParenthesisChecker.java

--- string/ParenthesisChecker.java
+++ string/ParenthesisChecker.java
@@ -3,7 +3,7 @@ class ParenthesisChecker
   {
     public static void main(String args[])
     {
-      Scanner sc=new Scanner(System.in);
+      Scanner sc=new Scanner(System.in); 
       String s=sc.nextLine();
       System.out.println(isBalanced(s));
     }
