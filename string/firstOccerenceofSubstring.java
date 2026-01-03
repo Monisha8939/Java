47beab0920dedc121cddb893a99f10af9bc5ad06
MONISHA K <k48172473@gmail.com>
2026-01-03 23:29:13 +0530

    firstOccerenceofSubstring.java

--- string/firstOccerenceofSubstring.java
+++ string/firstOccerenceofSubstring.java
@@ -3,7 +3,7 @@ public class FirstOccurenceofSubstring {
 	public static void main(String args[]) {
 		Scanner s=new Scanner(System.in);
 		String str=s.nextLine(); //Give the input for String
-		String str1=s.nextLine(); //Give the input for Substring
+		String str1=s.nextLine(); //Give the input for Substring 
 		int index=str.indexOf(str1);  // Find the index of the first occurrence of str1 in str
 		if(index!=-1)  // If the substring is found, print its index
     {
