751671c6b3eb8a349ffcbc42b6a3a28b4c6de53c
MONISHA K <k48172473@gmail.com>
2026-01-12 23:24:26 +0530

    EvenorOdd.java

--- Numbers/EvenorOdd.java
+++ Numbers/EvenorOdd.java
@@ -6,7 +6,7 @@ class demo
     Scanner s=new Scanner(System.in); 
     int n=s.nextInt(); // get input from User 
     if(n%2==0) // if Remainder is 0 then even else odd
-    {
+    { 
        System.out.println("Even");
      }
      else
