60e9495a62d5ab2f24e4437052ec2c0d149890f2
MONISHA K <k48172473@gmail.com>
2026-01-13 23:20:00 +0530

    DuplicataValues.java

--- Array/DuplicataValues.java
+++ Array/DuplicataValues.java
@@ -8,7 +8,7 @@ class A
       int n=s.nextInt(); //get the  size of the array
       int arr[]=new int[n]; //delcare the array size
       for(int i=0;i<n;i++)
-        {
+        { 
           arr[i]=s.nextInt();//get the input from the usere 
         }
       for(int i=0;i<arr.length;i++) //check if the duplicate element is found or not
