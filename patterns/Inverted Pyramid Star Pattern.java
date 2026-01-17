49d9f56f51d475984d679dac6ffd554607d717d3
MONISHA K <k48172473@gmail.com>
2026-01-17 23:06:02 +0530

    Inverted Pyramid Star Pattern.java

--- patterns/Inverted Pyramid Star Pattern.java
+++ patterns/Inverted Pyramid Star Pattern.java
@@ -3,7 +3,7 @@ class A{
   public static void main(String args[]){
     Scanner s=new Scanner(System.in);
     int n=s.nextInt();
-    for(int i=1;i<=n;i++){ 
+    for(int i=1;i<=n;i++){  
             for(int j=1;j<i;j++){
                 System.out.print(" "); 
             }
