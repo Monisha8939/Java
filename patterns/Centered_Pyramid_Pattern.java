2f162e9d63fdd209d31f1554ef806ec59701130b
MONISHA K <k48172473@gmail.com>
2026-01-21 15:35:25 +0530

    Centered Pyramid Pattern.java

--- patterns/Centered_Pyramid_Pattern.java
+++ patterns/Centered_Pyramid_Pattern.java
@@ -6,7 +6,7 @@ class A{
     for(int i=1;i<=n;i++){
       for(int j=i;j<n;j++){
         System.out.print("  ");
-      }
+      } 
       for(int k=1;k<=(2*i-1);k++){
         System.out.print("* ");
       }
